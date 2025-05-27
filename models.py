3ef2bffe93562e6386b335a3aaa479568c53210e
krss <krss@krsss-MacBook-Pro.local>
2025-05-26 17:25:56 -0700

    Initial commit: FastAPI + PostgreSQL Dockerized app

---
+++ models.py
@@ -0,0 +1,19 @@
+# app/models.py
+
+from sqlalchemy import Column, Integer, String
+from .database import Base
+
+class Item(Base):
+    __tablename__ = "items"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, index=True)
+    description = Column(String)
+
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True, index=True, nullable=False)
+    hashed_password = Column(String, nullable=False)
+
+
